ca5c2a3c35c1339602090bcddb0ca4df6dcd129c
danfragoso <danilo.fragoso@gmail.com>
2021-07-26 20:47:22 -0300

    update

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -9,18 +9,21 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "JetBrains Mono:size=12", "Noto Color Emoji:size=11" };
+static const char dmenufont[]       = "JetBrains Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//static const char col_cyan[]        = "#708090";
+static const char col_cyan[]        = "#eeeeee";
+static const char col_black[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray4, col_black, col_black },
+	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+	[SchemeTitle]  = { col_gray4, col_black,  col_black  },
 };
 
 /* tagging */
@@ -43,9 +46,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "🧐",      tile },    /* first entry is default */
+	{ "🥴",      NULL },    /* no layout function means floating behavior */
+	{ "🐸",      monocle },
 };
 
 /* key definitions */
@@ -61,10 +64,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray4, "-sb", col_cyan, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
+static const char *brightUp[]  = { "brightnessctl", "-n", "s", "+5%", NULL };
+static const char *brightDown[]  = { "brightnessctl", "-n", "s", "5%-", NULL };
+
 static Key keys[] = {
+	{ 0,                            0x1008ff02,      spawn,          {.v = brightUp } },
+	{ 0,                            0x1008ff03,      spawn,          {.v = brightDown } },
+
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
